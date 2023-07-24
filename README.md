82cad605249831f97b3f07ed9c43d23309415d39
Xiaoran <xc2422@columbia.edu>
2023-07-23 19:15:50 -0700

    upload the prio version

--- README.md
+++ README.md
@@ -1,1 +1,191 @@
-# crytoDashboard2.0
+# Crypto Dashboard
+
+## Description
+
+<<<<<<< HEAD
+=======
+[Visit the Deployed Site](https://ailllycxr.github.io/cryptoDashboard/)
+
+>>>>>>> 015f1f33b882f670a7f4bd44a137b256457e7f10
+Crypto Dashboard is a web application that provides real-time information and analysis for various cryptocurrencies. It offers features such as displaying the current price of cryptocurrencies, calculating profit based on investment and percentage allocations, showing market sentiment and trading volume, and providing historical data and conversion types. The application aims to help users make informed decisions and track the performance of their cryptocurrency investments.
+
+The user interface is designed with a clean and modern layout, making it easy to navigate and understand the information presented. The site incorporates responsive design principles, ensuring a seamless experience across different devices and screen sizes.
+
+Overall, Crypto Dashboard is a valuable tool for cryptocurrency enthusiasts and investors who want to stay updated with the latest market trends and make data-driven decisions.
+
+## Technologies
+<<<<<<< HEAD
+=======
+
+| Technology Used         | Resource URL           | 
+| ------------- |:-------------:| 
+| HTML    | [https://developer.mozilla.org/en-US/docs/Web/HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) | 
+| CSS     | [https://developer.mozilla.org/en-US/docs/Web/CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)      |   
+| Git | [https://git-scm.com/](https://git-scm.com/)     |  
+| Javascript | [https://developer.mozilla.org/en-US/docs/Web/JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript) |
+| JQuery | [https://api.jquery.com/category/attributes/](https://api.jquery.com/category/attributes/)
+| BootStrap    | [https://getbootstrap.com/docs/5.3/getting-started/introduction/](https://getbootstrap.com/docs/5.3/getting-started/introduction/) |
+| UIKit    | [https://getuikit.com/docs/introduction](https://getuikit.com/docs/introduction) |
+| CryptoCompare API    | [https://min-api.cryptocompare.com/documentation](https://min-api.cryptocompare.com/documentation) |
+| CoinLayer API    | [https://coinlayer.com/documentation](https://coinlayer.com/documentation) |
+>>>>>>> 015f1f33b882f670a7f4bd44a137b256457e7f10
+
+| Technology Used   |                                                            Resource URL                                                            |
+| ----------------- | :--------------------------------------------------------------------------------------------------------------------------------: |
+| HTML              |               [https://developer.mozilla.org/en-US/docs/Web/HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)               |
+| CSS               |                [https://developer.mozilla.org/en-US/docs/Web/CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)                |
+| Git               |                                            [https://git-scm.com/](https://git-scm.com/)                                            |
+| Javascript        |         [https://developer.mozilla.org/en-US/docs/Web/JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)         |
+| JQuery            |                     [https://api.jquery.com/category/attributes/](https://api.jquery.com/category/attributes/)                     |
+| BootStrap         | [https://getbootstrap.com/docs/5.3/getting-started/introduction/](https://getbootstrap.com/docs/5.3/getting-started/introduction/) |
+| UIKit             |                          [https://getuikit.com/docs/introduction](https://getuikit.com/docs/introduction)                          |
+| CryptoCompare API |                 [https://min-api.cryptocompare.com/documentation](https://min-api.cryptocompare.com/documentation)                 |
+| CoinLayer API     |                             [https://coinlayer.com/documentation](https://coinlayer.com/documentation)                             |
+
+## Code Refactor Example
+
+```HTML
+   <div id="visDivFour" class="uk-animation-slide-right-medium">
+                  <h5 class="uk-margin-bottom">Choose your crypto's and weights (max 5)</h5>
+                  <input id="coin1" class="uk-input uk-form-width-medium uk-form-small" type="text" placeholder="ie: BTC" aria-label="Small">
+                  <input value="0" id="percent1" class="uk-input uk-form-width-small uk-form-small" type="number" placeholder="5 (%)" aria-label="Small">
+                  <br>
+                  <input id="coin2" class="uk-input uk-form-width-medium uk-form-small" type="text" placeholder="ie: ETH" aria-label="Small">
+                  <input value="0" id="percent2" class="uk-input uk-form-width-small uk-form-small" type="number" placeholder="10 (%)" aria-label="Small">
+                  <br>
+                  <input id="coin3" class="uk-input uk-form-width-medium uk-form-small" type="text" placeholder="ie: USDT" aria-label="Small">
+                  <input value="0" id="percent3" class="uk-input uk-form-width-small uk-form-small" type="number" placeholder="20 (%)" aria-label="Small">
+                  <br>
+                  <input id="coin4" class="uk-input uk-form-width-medium uk-form-small" type="text" placeholder="ie: XRP" aria-label="Small">
+                  <input value="0" id="percent4" class="uk-input uk-form-width-small uk-form-small" type="number" placeholder="30 (%)" aria-label="Small">
+                  <br>
+                  <input id="coin5" class="uk-input uk-form-width-medium uk-form-small" type="text" placeholder="ie: SOL" aria-label="Small">
+                  <input value="0" id="percent5" class="uk-input uk-form-width-small uk-form-small" type="number" placeholder="35 (%)" aria-label="Small">
+                  <br>
+                  <div class=" uk-margin-top">
+                      <button class="submit-button">make portfolio!</button>
+                  </div>
+              </div>
+```
+
+```CSS
+*{
+    margin: 0 !important;
+    padding: 0 !important;
+    font-family: 'Titillium Web', sans-serif !important;
+    letter-spacing: 2px !important;
+    color: white !important;
+    background-color: black !important;
+}
+input{
+    background-color: black !important;
+}
+html{
+    color: black !important;
+}
+h1, h2{
+    color: white !important;
+    padding: 10px !important;
+    border-bottom: 2px solid white;
+    display: table !important;
+}
+#surveyForm{
+    width: 100% !important;
+}
+#survey{
+    width: 80% !important;
+    padding-left: 50px !important;
+    border-left: 2px solid white;
+}
+
+```
+
+```Java
+function getSocialSentiment(coinList){
+    var index=0
+    for (var i=0;i<coinList.length;i++){
+        var coinName= coinList[i]
+        var requestUrl="https://min-api.cryptocompare.com/data/tradingsignals/intotheblock/latest?fsym="+coinName+"&api_key=4643002f15269f3fab2e433e581986eb8e0d2eb7711e7b78e90fb547713396df";
+        fetch(requestUrl)
+            .then(function(response){
+                if (!response.ok){
+                    showError();
+                }else{
+                    return response.json();
+                }
+            })
+            .then(function(responseData){
+            var sentiment = responseData.Data.addressesNetGrowth.sentiment;
+            var paragraph = $(".crypto"+index);
+            var pTag=$("<p>")
+            pTag.addClass("p1")
+            pTag.text("market: "+sentiment)
+            paragraph.append(pTag)
+            index++
+            })
+        }
+    }
+
+```
+
+## Usage 
+
+### The screenshots below demonstrate the application's functionality.
+
+<img src="assets/welcome.png" height="450" width="720" >
+
+### - Our Welcome Page displays the App Name as well as a welcome message and a begin button.
+### - It also features a moving cloud animation at the bottom of the browser.
+
+<img src="assets/getinfo-survery.png" height="450" width="720" >
+
+### - Upon pressing begin, the user is taken to the main page.
+### - On this page, the user can read a descriptive summary on coins of their choice.
+### - They then can use their knowledge to create a custom portfolio.
+
+<img src="assets/portfolio.png" height="450" width="720" >
+
+### - After completing the survey, a custom portfolio is generated.
+### - Here the user can view the overall performance on their portfolio as
+### well as view the performace and metrics of each coin they invested in
+### - The amount and depth of information displayed for each coin is
+### generated according to the experience level indicated by the user.
+
+<img src="assets/conversion.png" height="450" width="720" >
+
+### - The user also has the ability to find conversion rates between two coins by
+### entering one coin name with the amount to convert and a second to convert to
+
+
+# Author Info
+
+![Final Product](./assets/img/Front%20Page.png)
+![Final Product](./assets/img/Main%20Page.png)
+
+### Jordan Lopez
+
+- [LinkedIn](https://www.linkedin.com/in/jordanlopez123/)
+- [Github](https://github.com/Lopez-Jordan)
+
+### Xiaoran Cai
+
+- [LinkedIn](https://www.linkedin.com/in/xrcai/)
+- [Github](https://github.com/Ailllycxr)
+
+### Jesus Thomas Reyes
+
+- [LinkedIn](https://www.linkedin.com/in/jesus-thomas-reyes-aa001a192/)
+- [Github](https://github.com/jesustgr)
+
+## Credits
+
+- W3school| [https://www.w3schools.com](https://www.w3schools.com)
+
+## License
+
+Copyright (c). All rights reserved.
+Licensed under the [MIT](https://choosealicense.com/licenses/mit/) license.
+
+## Features
+
+The nevigation bar items link to their content repectively
---
+++ assets/script.js
@@ -0,0 +1,421 @@
+init();
+function init(){
+  if(localStorage.length > 0){
+    var name = JSON.parse(localStorage.getItem(localStorage.key(0))).userName;
+    if (typeof name === "undefined"){
+      return;
+    }
+    $("#greet").css("display","block");
+    $("#greet").text("Welcome back, " + name);
+    return;
+  }
+  else{
+    return;
+  }
+}
+
+
+
+var portfolioProfit=0
+$("#beginBtn").on("click", function(){ // button to take from welcome page to main page
+    location.replace("main.html");
+});
+function validateForm(data){ // validating the survey object for correct user input
+  if (data.userName == ""){
+    UIkit.modal('#noName').show();
+    return false;
+  }
+  if (isNaN(data.totalInvestment)){
+    UIkit.modal('#noInvestment').show();
+    return false;
+  }
+  if (data.totalInvestment < 0 || data.totalInvestment > 100000000){
+    UIkit.modal('#ivalidInvestment').show();
+    return false;
+  }
+  var totalPercent = 0;
+  for (var i=0; i<5; i++){
+    totalPercent += data.coins[i].percent;
+  }
+  if (isNaN(totalPercent)){
+    UIkit.modal('#makeZero').show();
+    return false;
+  }
+  if (totalPercent != 100){
+    UIkit.modal('#makeOneHundred').show();
+    return false;
+  }
+  return true;
+}
+// submitting and validating the form itself
+$("#surveyForm").on("submit", function(e){
+  e.preventDefault(); // still haven't created object yet
+
+  var coin1Obj = {
+    symbol : $("#coin1").val(),
+    percent: parseInt($("#percent1").val())
+  }
+  var coin2Obj = {
+    symbol : $("#coin2").val(),
+    percent: parseInt($("#percent2").val())
+  }
+  var coin3Obj = {
+    symbol : $("#coin3").val(),
+    percent: parseInt($("#percent3").val())
+  }
+  var coin4Obj = {
+    symbol : $("#coin4").val(),
+    percent: parseInt($("#percent4").val())
+  }
+  var coin5Obj = {
+    symbol : $("#coin5").val(),
+    percent: parseInt($("#percent5").val())
+  }
+
+  var newObj = {
+    userName: $("#inputName").val(),
+    visited: true,
+    level: parseInt($("#inputLevel").val()),
+    totalInvestment : parseInt($("#investment").val()),
+    coins: [coin1Obj, coin2Obj, coin3Obj, coin4Obj, coin5Obj]
+  }
+
+  if (validateForm(newObj)){
+    localStorage.setItem(newObj.userName, JSON.stringify(newObj));
+    newUser = localStorage.getItem(newObj.userName)
+    var newUserParse=JSON.parse(newUser)
+    createUser(newObj)
+    $("#totalClass").css("display","block");
+  }
+
+  $("#visDivOne").css("display","none");
+  $("#visDivTwo").css("display","none");
+  $("#visDivThree").css("display","none");
+  $("#visDivFour").css("display","none");
+  $("#create").css("display","block");
+  $("#nextOne").css("display","block");
+  $("#nextTwo").css("display","block");
+  $("#nextThree").css("display","block");
+
+  $("#last").css("display","block");
+});
+// creating the dynamic parts of the form
+$("#create").on("click", function(){
+  $("#visDivOne").css("display","block");
+  $("#create").css("display","none");
+});
+$("#nextOne").on("click",function(){
+  $("#visDivTwo").css("display","block");
+  $("#nextOne").css("display","none");
+});
+$("#nextTwo").on("click",function(){
+  $("#visDivThree").css("display", "block");
+  $("#nextTwo").css("display","none");
+});
+$("#nextThree").on("click",function(){
+  $("#visDivFour").css("display", "block");
+  $("#nextThree").css("display","none");
+});
+
+// starting the education section
+$("#getInfo").on("click",function(){
+  $("#content-div").css("display", "block");
+  
+  if ($("#coinInfo").val() == ""){
+    console.log("no coin selected");
+    UIkit.modal('#noCoinName').show();
+  }
+  var symbol = $("#coinInfo").val();
+  
+  var descURL = "https://data-api.cryptocompare.com/asset/v1/data/by/symbol?asset_symbol=" + symbol + "&api_key=4643002f15269f3fab2e433e581986eb8e0d2eb7711e7b78e90fb547713396df";
+  fetch(descURL)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+ // Log the entire data object to inspect its structure
+    $("#contentP").text(data.Data.ASSET_DESCRIPTION_SUMMARY);
+    
+    var imgUrl = data.Data.LOGO_URL;
+    var nameOfCoin = data.Data.NAME;
+    $("#coinLogo").attr('src', imgUrl);
+    $("#nameOfCoin").text(nameOfCoin);
+  })
+  .catch(function(){
+    UIkit.modal('#noValidCoin').show();
+  });
+  
+
+});
+
+
+function createUser(newUserParse){
+  var coinList=[]
+  for (var i =0; i<newUserParse.coins.length;i++){
+      newCoin=newUserParse.coins[i].symbol
+      if (newCoin!=""){
+      coinList.push(newCoin)}
+
+  }
+  var per=[]
+  for (var i =0; i<newUserParse.coins.length;i++){
+      percent=newUserParse.coins[i].percent
+      if (percent!=NaN){
+      per.push(percent)}
+  }
+  var level = newUserParse.level
+  var totalInvestment=newUserParse.totalInvestment
+  var profit=0
+  var pastDayEarningRateHighSum=[]
+  var profitList=[]
+  
+  populateCoinName(coinList)
+  User(level,coinList,per,totalInvestment)
+}
+
+
+function User(level,coinList,per,totalInvestment){
+  if (level===1){
+  getProfit(coinList,per,totalInvestment)
+  getTradingVolume(coinList)
+  getSocialSentiment(coinList)
+  }else if(level===2){
+  getProfit(coinList,per,totalInvestment)
+  getTradingVolume(coinList)
+  getSocialSentiment(coinList)
+  conversionType(coinList)
+  } else{
+  getProfit(coinList,per,totalInvestment)
+  getTradingVolume(coinList)
+  getSocialSentiment(coinList)
+  conversionType(coinList)
+  volumeFromTo(coinList)   
+  }
+  
+}
+//Part 1: Populate Coin Name
+function populateCoinName(coinList){
+    for(var i=0; i<coinList.length; i++){
+        var title = $(".crypto"+[i]).append("<title></title>")
+        title.text([i+1]+". "+coinList[i])
+     
+    }
+
+}
+
+//Individual cards
+//Part2: Current Price
+//get data from Api, caculate profit, populate to the website
+function getPriceApi(coinList){ 
+    var index=0;
+    for (var i=0; i<coinList.length; i++){
+        var coinName= coinList[i];
+        var requestUrl="https://min-api.cryptocompare.com/data/price?fsym="+coinName+"&tsyms=USD,JPY,EUR&api_key=4643002f15269f3fab2e433e581986eb8e0d2eb7711e7b78e90fb547713396df";
+        fetch(requestUrl)
+            .then(function(response){
+                if (!response.ok){
+                    showError();
+                }else{
+                    return response.json();    
+                }
+            })
+            .then(function(responseData){
+                var price = Math.round(responseData.USD); 
+                var paragraph = $(".crypto"+index);
+                var pTag=$("<p>")
+                pTag.addClass("p1")
+                pTag.text("price: "+price + " $")
+                paragraph.append(pTag)
+                index++
+            })      
+    }
+ }
+//Part2: Social Sentiment 
+function getSocialSentiment(coinList){
+    var index=0
+    for (var i=0;i<coinList.length;i++){
+        var coinName= coinList[i]
+        var requestUrl="https://min-api.cryptocompare.com/data/tradingsignals/intotheblock/latest?fsym="+coinName+"&api_key=4643002f15269f3fab2e433e581986eb8e0d2eb7711e7b78e90fb547713396df";
+        fetch(requestUrl)
+            .then(function(response){
+                if (!response.ok){
+                    showError();
+                }else{            
+                    return response.json();  
+                }
+            })
+            .then(function(responseData){
+            console.log(responseData)
+            }) 
+        }     
+    }
+
+//Part3: Profit
+//get data from Api, caculate profit, populate to the website
+function getProfit(coinList,per,totalInvestment){ 
+    // console.log(coinList)
+    var index=0;
+    for (var i=0; i<coinList.length; i++){
+
+        
+
+        var coinName= coinList[i];
+        var requestUrl="https://min-api.cryptocompare.com/data/generateAvg?fsym="+coinName+"&tsym=USD&e=Kraken&api_key=4643002f15269f3fab2e433e581986eb8e0d2eb7711e7b78e90fb547713396df";
+        fetch(requestUrl)
+            .then(function(response){
+                if (!response.ok){
+                    showError();
+                }else{
+                    return response.json();    
+                }
+            })
+            .then(function(responseData){
+                // console.log(responseData)
+                var ChangePCT = responseData.RAW.CHANGEPCT24HOUR
+                profit = Math.round(ChangePCT*totalInvestment*per[i])
+                var paragraph = $(".crypto"+index);
+                var pTag=$("<p>")
+                pTag.text("profit: $ "+profit)
+                paragraph.append(pTag)
+
+                var cardboard = $("#card"+index);
+                console.log(cardboard)
+                cardboard.addClass("p-3 border border-info");
+
+                index++
+                //populate the porfolio cards
+                portfolioProfit+=profit
+                // console.log(profolioProfit)
+
+                var portfolioTestment = $("#totalInvestment");
+                portfolioTestment.text("Investment: $ "+totalInvestment)
+
+                var totalProfit = $("#portfolioProfit");
+                if (portfolioProfit < 0){
+                    totalNetWorth = 0;
+                }
+                totalProfit.text("Profit: $ "+ portfolioProfit)
+
+                var totalNetWorth =totalInvestment+portfolioProfit
+                var netWorth = $("#totalNetWorth");
+                if (totalNetWorth < 0){
+                    
+                }
+                netWorth.text("Net Worth: $ "+ totalNetWorth)
+
+                
+            })
+    }
+ }
+
+
+//Part4: Trading volume
+function getTradingVolume(coinList){
+    var index=0
+    for (var i=0;i<coinList.length;i++){
+        var coinName= coinList[i]
+        var requestUrl="https://min-api.cryptocompare.com/data/v2/histoday?fsym="+coinName+"&tsym=USD&limit=10&api_key=4643002f15269f3fab2e433e581986eb8e0d2eb7711e7b78e90fb547713396df";
+        fetch(requestUrl)
+            .then(function(response){
+                if (!response.ok){
+                    showError();
+                }else{            
+                    return response.json();  
+                }
+            })
+            .then(function(responseData){
+                var allDayVolume=0;
+                for (var j = 0; j<11; j++){
+                    var hourClose = responseData.Data.Data[j].close; 
+                    allDayVolume+=hourClose;
+                }
+                var volumeAverage = Math.round(allDayVolume/11);
+                var paragraph = $(".crypto"+index);
+                var pTag=$("<p>")
+                pTag.addClass("p2")
+                pTag.text("hrly avg vol (close): " +volumeAverage)
+                paragraph.append(pTag)
+                index++
+            }) 
+        }     
+    }
+
+//Part5: conversionType
+function conversionType(coinList){
+    var index=0
+    for (var i=0;i<coinList.length;i++){
+        var coinName= coinList[i]
+        var requestUrl="https://min-api.cryptocompare.com/data/v2/histoday?fsym="+coinName+"&tsym=USD&limit=10&api_key=4643002f15269f3fab2e433e581986eb8e0d2eb7711e7b78e90fb547713396df";
+        fetch(requestUrl)
+            .then(function(response){
+                if (!response.ok){
+                    showError();
+                }else{            
+                    return response.json();  
+                }
+            })
+            .then(function(responseData){
+                var type = responseData.Data.Data[0].conversionType
+                var paragraph = $(".crypto"+index);
+                var pTag=$("<p>")
+                pTag.addClass("p2")
+                pTag.text("conversion type: " +type)
+                paragraph.append(pTag)
+                index++
+            }) 
+        }     
+    }
+//Part6: Volume from,to
+    function volumeFromTo(coinList){
+        var index=0
+        for (var i=0;i<coinList.length;i++){
+            var coinName= coinList[i]
+            var requestUrl="https://min-api.cryptocompare.com/data/v2/histoday?fsym="+coinName+"&tsym=USD&limit=10&api_key=4643002f15269f3fab2e433e581986eb8e0d2eb7711e7b78e90fb547713396df";
+            fetch(requestUrl)
+                .then(function(response){
+                    if (!response.ok){
+                        showError();
+                    }else{            
+                        return response.json();  
+                    }
+                })
+                .then(function(responseData){
+                    var volumeFrom = Math.round(responseData.Data.Data[0].volumefrom)
+                    var volumeTo = Math.round(responseData.Data.Data[0].volumeto)
+                    var paragraph = $(".crypto"+index);
+                    var pTag1=$("<p>")
+                    pTag1.addClass("p2")
+                    pTag1.text("vol (open): " +volumeFrom)
+                    var pTag2=$("<p>")
+                    pTag2.addClass("p3")
+                    pTag2.text("vol (close): " +volumeTo)
+                    paragraph.append(pTag1)
+                    paragraph.append(pTag2)
+                    index++
+                }) 
+            }     
+        }
+
+
+
+$("#conversionButton").on("click",function(){
+  
+
+  var input = $("#coinInput1").val();
+  console.log(input);
+  var amount = $("#amtInput").val();
+  var output = $("#coinInput2").val();
+  var requestUrl="https://api.coinlayer.com/convert?access_key=aba7605942c68749d9255a7a4124507a"+"&from="+input+"&to="+output+"&amount="+amount
+  console.log(requestUrl);
+  fetch(requestUrl)
+      .then(function(response){          
+          return response.json(); 
+      })
+      .then(function(data){
+          var coinConversion = data.result
+          $('#amtOutput').text(amount + " " + input + "  =  " + coinConversion + " " + output);
+
+      });
+})
+
+"1 Bitcoin = 0.13 eth"
